522641b334c840fbf913714a87ac92484f65f608
bryan <36523635+bbrandt-podo@users.noreply.github.com>
2018-03-26 11:57:10 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var raml2json = require('ramldt2jsonschema');
+var port = 8080;
+var app = express();
+
+var jsonInput;
+
+
+var server = app.listen(port, listening);
+
+function listening(req, res) {
+  console.log("Server is listening. . .");
+}
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+app.get('*', mainGet);
+app.post('*', mainPost);
+
+
+
+function mainGet(req, res) {
+
+
+  res.end();
+
+}
+
+
+function mainPost(req, res) {
+
+  if (req.url == '/convert') {
+    jsonInput = req.body;
+    jsonInput = convertToRAML(jsonInput);
+  } else {
+    data = null;
+  }
+
+  res.send(jsonInput);
+  res.end();
+
+}
+
+function convertToRAML(jsonInput) {
+
+jsonInput = JSON.stringify(jsonInput);
+var ramlData;
+raml2json.js2dt(jsonInput, 'Person', function(err, raml){
+  if (err){
+    console.log(err);
+    return;
+  }
+  console.log('#%RAML 1.0 Library\n');
+  console.log(raml);
+  ramlData = raml;
+});
+
+  return ramlData;
+}
